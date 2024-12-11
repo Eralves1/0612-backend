6ee5e008964b52d848f8534248e0723d9b13e91b
EUDANTAS RODRIGUES ALVES <eudantas.ralves@senacsp.edu.br>
2024-12-10 21:38:48 -0300

    ter-10122024

---
+++ README.md
@@ -0,0 +1,5 @@
+# API de Carros - webAPI
+
+Esta é uma API RESTFUL desenvolvida para o gerenciamento de informações de carros, utilizando **Node.js** e **Express**. A API permite criar, ler, atualizar e excluir carros, com validação dos dados utilizando a biblioteca **Joi**.
+
+Este é um projeto básico de CRUD (Create, Read, Update, Delete), que será expandido no futuro, Este é apenasd o escopo inicial.
--- validacao.js
+++ validacao.js
@@ -3,31 +3,31 @@ import Joi from "joi";
 
 // Validação para inclusão carro (modelo)
 export const modeloCarro = Joi.object({
-    nome: Joi.string().min(3).required().message({
+    nome: Joi.string().min(3).required().messages({
         'string.min': 'O nome do carro deve ter pelo menos 3 caracteres.',
         'any.required': 'O nome do carro é Obrigatório.',
     }), // Nome do carro Min. 3 Caracter
-    sigla: Joi.string().length(3).required().message({
+    sigla: Joi.string().length(3).required().messages({
         'string.length': 'A sigla deve ter exatamente 3 caracteres.',
         'any.required': 'A sigla é obrigatória.',
     }),  //Sigla do carro, 3 caracter
-    potencia: Joi.number().min(1).required().message({
+    potencia: Joi.number().min(1).required().messages({
         'number.min': 'A Potência deve ser maior ou igual a 1.',
         'any.required': 'A Potência é obrigatória.',
     }), //Potência mínima de 1 CV
-    velocidadeMaxima: Joi.number().min(1).required().message({
+    velocidadeMaxima: Joi.number().min(1).required().messages({
         'number.min': 'A velocidade máxima deve ser maior ou igual a 1.',
         'any.required': 'A velocidade máxima é obrigatória.',
     }), //Velocidade min. de 1
-    consumo: Joi.number().min(0.1).required().message({
+    consumo: Joi.number().min(0.1).required().messages({
         'number.min': 'O consumo deve ser maior ou igual a 0.1',
         'any.required': 'o consumo é obrigatório.',
     }), //Consumo mínimo de 0.1
-    aceleracao: Joi.number().min(0).required().message({
+    aceleracao: Joi.number().min(0).required().messages({
         'number.min': 'A aceleração deve ser maior ou igual a 0.',
         'any.required': 'A aceleração é obrigatória.',
     }), //Aceleração min. de 0
-    preco: Joi.number().min(0).required().message({
+    preco: Joi.number().min(0).required().messages({
         'number.min': 'O preço deve ser maior ou igual a 1.',
         'any.required': 'O preço é obrigatório.',
 
